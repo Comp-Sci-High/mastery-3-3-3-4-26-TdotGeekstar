d2448645f4c422b327ec4015e194ac5814994003
TdotGeekstar <tom.saoura26@compscihigh.org>
2026-02-24 06:15:34 +0000

    completion

--- index.js
+++ index.js
@@ -8,20 +8,37 @@ const app = express();
  * It runs for every request before reaching the route handlers
  */
 
+app.use((req, res, next) => {
+    console.log(`${req.method} request to ${req.url}`)
+    next()
+})
+
 /**
  * Add a route to /name that displays your name as plain text
  */
 
+app.get('/name', (req, res) => {
+    res.send('Tom Saoura')
+})
 
 /**
  * Add a route to /about that displays a JSON object with details describing yourself
  */
 
-
+app.get('/about', (req, res) => {
+    res.json({
+        age: 16,
+        hobby: 'coding',
+        school: 'Hoboken High'
+    })
+})
 /**
  * Add 404 Middleware 
  */
 
+app.use((req, res) => {
+    res.status(404).send('404 Not Found')
+})
 
 app.listen(3000, () => {
     console.log(`Server is running.`);
